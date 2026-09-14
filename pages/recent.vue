12c8dedee7309305e9d4e02b8254d37c6d74d15a
jlpereira <pepelui@outlook.com>
2026-09-14 18:45:29 -0300

    Add recent page and navbar links

---
+++ pages/recent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container mx-auto py-6">
+    <h1 class="text-4xl font-bold mb-6">Recent</h1>
+    <div class="flex flex-col gap-4">
+      <RecentTaxonTable
+        :parameters="{
+          validity: true,
+          recent: true,
+          taxon_name_id: [909345],
+          descendants: true,
+          taxon_name_type: 'Protonym',
+          recent_target: 'created_at'
+        }"
+      />
+
+      <RecentTable
+        :attributes="{
+          cached: 'Source'
+        }"
+        route="/sources"
+        :parameters="{
+          in_project: true,
+          recent: true,
+          recent_target: 'created_at'
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import RecentTable from './components/RecentTable.vue'
+import RecentTaxonTable from './components/RecentTaxonTable.vue'
+</script>
